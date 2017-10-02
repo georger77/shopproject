c26280c23937bf50f6633a81930a0f36721bd995
George Rezvanov <rezvanov@ukr.net>
2017-10-02 13:36:11 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,6 +1,37 @@
-setInterval(function() {
-    if (contacts.hidden) {contacts.hidden = false; }
-}else {
-    contacts.hidden = true;
+// setInterval(function() {
+//     if (contacts.hidden) {contacts.hidden = false; }
+// }else {
+//     contacts.hidden = true;
+// }
+// },1000 );
+
+
+
+
+
+document.getElementById('page-loaded').innerHTML = (new Date()).toLocaleTimeString();
+document.getElementById('btn-promotion').addEventListener('click', checkPromotion);
+document.getElementById('send-information').addEventListener('click', answerForMessage);
+
+function checkPromotion() {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const data = JSON.parse(xhr.responseText);
+            document.getElementById('action').innerHTML = data['action'];
+        }
+    }
+    xhr.open('GET', 'promotion.json', true);
+    xhr.send();
 }
-},1000 );
+function answerForMessage() {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const data = JSON.parse(xhr.responseText);
+            document.getElementById('answer').innerHTML = data['answer'];
+        }
+    }
+    xhr.open('GET', 'answer-for-send.json', true);
+    xhr.send();
+}
